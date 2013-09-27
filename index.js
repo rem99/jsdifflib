37740e3abfcdcdc65c6c0785ecd826ecf17f179d
Forbes Lindesay <forbes.lindesay@red-gate.com>
2013-09-27 11:44:26 +0100

    Make the row have the class for inline diff

--- index.js
+++ index.js
@@ -510,9 +510,10 @@ var difflib = module.exports = {
     }
     
     function addCellsInline (row, tidx, tidx2, textLines, change) {
+      row.className = change;
       row.appendChild(telt("th", tidx == null ? "" : (tidx + 1).toString()));
       row.appendChild(telt("th", tidx2 == null ? "" : (tidx2 + 1).toString()));
-      row.appendChild(ctelt("td", change, textLines[tidx != null ? tidx : tidx2].replace(/\t/g, "\u00a0\u00a0\u00a0\u00a0")));
+      row.appendChild(telt("td", textLines[tidx != null ? tidx : tidx2].replace(/\t/g, "\u00a0\u00a0\u00a0\u00a0")));
     }
     
     for (var idx = 0; idx < opcodes.length; idx++) {
